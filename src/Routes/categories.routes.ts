d1a438366bc578c87d1d3458439ec486fe791cde
caio.buteri <caio.buteri@virtuspay.com.br>
2022-01-26 23:44:41 -0300

    Creation of Categories Repository using SOLID

--- src/Routes/categories.routes.ts
+++ src/Routes/categories.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from "express";
-import { Category } from "../model/Category";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  return response.status(201).send();
+});
 
-  categories.push(category);
+categoriesRoutes.get("/", (_, response) => {
+  const categories = categoriesRepository.list();
 
-  return response.status(201).json({ category });
+  return response.status(200).json(categories);
 });
 
 export { categoriesRoutes };

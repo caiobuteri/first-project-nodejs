f1deecc2218a490ec6444ed19e068c1cd622a5ab
caio.buteri <caio.buteri@virtuspay.com.br>
2022-01-27 21:11:05 -0300

    specifications routes added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 
 import { categoriesRoutes } from "./Routes/categories.routes";
+import { specificationRoutes } from "./Routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 
 app.listen(3335, () => console.log("Server is running!"));

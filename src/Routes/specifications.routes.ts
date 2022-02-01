51f2eebb6304ca57bd5386a5d83ff2da9d7dd044
caio.buteri <caio.buteri@virtuspay.com.br>
2022-02-01 20:46:37 -0300

    create categories route refactored

--- src/Routes/specifications.routes.ts
+++ src/Routes/specifications.routes.ts
@@ -1,27 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationServices = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationServices.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-specificationRoutes.get("/", (_, response) => {
-  const specifications = specificationsRepository.list();
-
-  return response.status(200).json(specifications);
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationRoutes };

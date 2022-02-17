b44a9d411deb40920e6acbe49f5d5452c1b5a550
Caio.Buteri <caiobuteri10@gmail.com>
2022-02-17 14:07:48 -0300

    creating specifications migration

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -6,9 +6,12 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-  create({ name, description }: ICreateSpecificationDTO): void;
-  findByName(name: string): Specification;
-  list({ name, description }: ICreateSpecificationDTO): Specification[];
+  create({ name, description }: ICreateSpecificationDTO): Promise<void>;
+  findByName(name: string): Promise<Specification>;
+  list({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
